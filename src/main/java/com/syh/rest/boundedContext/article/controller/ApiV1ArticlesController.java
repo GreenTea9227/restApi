a83a990e6b3dbfdc8b347e9910a817c16732c2fd
yohan <sjohn9227@naver.com>
2023-05-18 15:29:56 +0900

    article write 구현

--- src/main/java/com/syh/rest/boundedContext/article/controller/ApiV1ArticlesController.java
+++ src/main/java/com/syh/rest/boundedContext/article/controller/ApiV1ArticlesController.java
@@ -3,15 +3,21 @@ package com.syh.rest.boundedContext.article.controller;
 import com.syh.rest.base.rsData.RsData;
 import com.syh.rest.boundedContext.article.entity.Article;
 import com.syh.rest.boundedContext.article.service.ArticleService;
+import com.syh.rest.boundedContext.member.entity.Member;
+import com.syh.rest.boundedContext.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +30,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class ApiV1ArticlesController {
 
     private final ArticleService articleService;
+    private final MemberService memberService;
 
     @AllArgsConstructor
     @Getter
@@ -58,6 +65,37 @@ public class ApiV1ArticlesController {
                 "S-1", "성공", new ArticlesResponse(article)))
                 .orElseGet(() -> RsData.of(
                 "F-1", "$d번 게시물은 존재하지 않습니다".formatted(id), null));
+
+    }
+
+
+    @Data
+    public static class WriteRequest {
+        @NotBlank
+        private String subject;
+        @NotBlank
+        private String content;
+    }
+
+    @PostMapping("")
+    @Operation(summary = "등록", security = @SecurityRequirement(name = "bearerAuth"))
+    public RsData<WriteResponse> write(@Valid @RequestBody WriteRequest writeRequest,
+                                               @AuthenticationPrincipal User user) {
+        Member member = memberService.findByUsername(user.getUsername()).orElseThrow(() -> new UsernameNotFoundException("no!!"));
+
+        RsData<Article> rsData = articleService.write(member, writeRequest.getSubject(), writeRequest.getContent());
+
+        if (rsData.isFail()) {
+            return (RsData) rsData;
+        }
+
+        return RsData.of(rsData.getResultCode(),rsData.getMsg(),new WriteResponse(rsData.getData()));
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class WriteResponse {
+        private final Article article;
 
     }
 }
